c0036aeeb2f87c12ecd5c1c6527ca7e39378fa71
Matt Carabine <matt.carabine@hotmail.co.uk>
2018-05-01 15:55:15 +0100

    Ensure price fluctuations follow a normal distributions

--- live_market.py
+++ live_market.py
@@ -27,7 +27,7 @@ while True:
     for row in results:
         stock_key = "stock:"+ (row['symbol'])
         # perturb the price and round it to 2 decimal places
-        price_multiplier = random.uniform (0.9,1.1)
+        price_multiplier = random.normalvariate(1, 0.025)
         new_price = float(row['price']) * price_multiplier
         new_price = round (new_price, 2)
         SDK_CLIENT.mutate_in(stock_key,
