084666678cf8ffe4ad1e013c11b6b5626eb8a06f
Matt Carabine <matt.carabine@hotmail.co.uk>
2018-06-09 19:27:50 +0000

    Make live market tolerant to failures

--- live_market.py
+++ live_market.py
@@ -18,18 +18,21 @@ SDK_CLIENT.timeout = 15
 
 
 while True:
-    print "."
-    results = SDK_CLIENT.n1ql_query(
-    'SELECT symbol,price FROM {} WHERE symbol IS NOT MISSING AND price IS NOT MISSING'.format(bucket_name, ))
-    for row in results:
-        stock_key = "stock:"+ (row['symbol'])
-        # perturb the price and round it to 2 decimal places
-        price_multiplier = random.normalvariate(1, 0.025)
-        if row['symbol'] == "CBSE" and price_multiplier < 1:
-            price_multiplier = 1
-        new_price = float(row['price']) * price_multiplier
-        new_price = round (new_price, 2)
-        SDK_CLIENT.mutate_in(stock_key,
+    try:
+        print "."
+        results = SDK_CLIENT.n1ql_query(
+        'SELECT symbol,price FROM {} WHERE symbol IS NOT MISSING AND price IS NOT MISSING'.format(bucket_name, ))
+        for row in results:
+            stock_key = "stock:"+ (row['symbol'])
+            # perturb the price and round it to 2 decimal places
+            price_multiplier = random.normalvariate(1, 0.025)
+            if row['symbol'] == "CBSE" and price_multiplier < 1:
+                price_multiplier = 1
+            new_price = float(row['price']) * price_multiplier
+            new_price = round (new_price, 2)
+            SDK_CLIENT.mutate_in(stock_key,
                             SD.upsert('price', new_price))
-
+    except Exception:
+        pass
+    
     time.sleep(8)
